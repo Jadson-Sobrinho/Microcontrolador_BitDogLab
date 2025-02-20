d56ea3750a915c5f2e53603159f51406966f4df7
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-02-20 20:19:02 -0300

    Configurações do Display

--- Duelo_Faroeste.c
+++ Duelo_Faroeste.c
@@ -35,9 +35,16 @@ const uint button_1 = 6;
 uint32_t last_press_time_0 = 0;  // Última ativação do botão_0
 uint32_t last_press_time_1 = 0;  // Última ativação do botão_1
 
+//Display OLED
+const uint I2C_SDA = 14;
+const uint I2C_SCL = 15;
+
+// Variáveis voláteis para sinalizar atualização do display (usadas na IRQ)
+volatile bool display_update_flag = false;
+volatile int display_message_type = 0; 
 
 
-// Interrupção do botão para ativar/desativar animação
+// Interrupção do botão
 static void gpio_irq_handler(uint gpio, uint32_t events) {
     uint32_t current_time = to_ms_since_boot(get_absolute_time()); // Obtém o tempo atual em ms
 
@@ -45,9 +52,16 @@ static void gpio_irq_handler(uint gpio, uint32_t events) {
     {
         if (current_time - last_press_time_0 < DEBOUNCE_TIME_MS) return; // Ignora se estiver dentro do tempo de debounce
         last_press_time_0 = current_time; // Atualiza o tempo do último acionamento
+        display_message_type = 1;           // Código para "Botão A pressionado"
+        display_update_flag = true;
+        
     } 
-    else 
+    else if(gpio == button_1)
     {
+        if (current_time - last_press_time_0 < DEBOUNCE_TIME_MS) return; 
+        last_press_time_0 = current_time; 
+        display_message_type = 2;           // Código para "Botão B pressionado"
+        display_update_flag = true;
         
        
     }
@@ -57,15 +71,13 @@ static void gpio_irq_handler(uint gpio, uint32_t events) {
 int main()
 {
     PIO pio = pio0;
-    uint32_t valor_led = 0;
-    double r = 0.0, b = 0.0, g = 0.0;
 
     set_sys_clock_khz(128000, false);
     stdio_init_all();
 
     // Configurar interrupção no botão
     gpio_set_irq_enabled_with_callback(button_0, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-    gpio_set_irq_enabled(button_1, GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled_with_callback(button_1, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     //inicializar o botão de interrupção - GPIO5
     gpio_init(button_0);
@@ -79,6 +91,52 @@ int main()
 
     uint offset = pio_add_program(pio, &pio_matrix_program);
     uint sm = pio_claim_unused_sm(pio, true);
-    pio_matrix_program_init(pio, sm, offset, OUT_PINO);    
+    pio_matrix_program_init(pio, sm, offset, OUT_PINO);
+    
 
+    // Inicialização do i2c
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    // Inicialização completo do OLED SSD1306
+    ssd1306_init();
+    
+    // Configura a área de renderização do display
+    struct render_area frame_area = {
+        .start_column = 0,
+        .end_column = ssd1306_width - 1,
+        .start_page = 0,
+        .end_page = ssd1306_n_pages - 1
+    };
+    calculate_render_area_buffer_length(&frame_area);
+
+    // Cria um buffer para o display e limpa o display
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    while (true) {
+            //memset(ssd, 0, ssd1306_buffer_length);  // Limpa o display
+            char msg[26];  // Buffer para a mensagem a ser exibida
+            render_on_display(ssd, &frame_area);  // Atualiza o display
+        
+    
+        // Se a flag de atualização do display estiver setada, atualiza o display com a mensagem
+        if (display_update_flag) {
+            memset(ssd, 0, ssd1306_buffer_length);
+            
+            if (display_message_type == 1) {
+                ssd1306_draw_string(ssd, 5, 0, "Player 1 Venceu");
+            } 
+            else if (display_message_type == 2) {
+                ssd1306_draw_string(ssd, 5, 0, "Player 2 Venceu");
+            } 
+
+            render_on_display(ssd, &frame_area);  // Atualiza o display
+            display_update_flag = false; // Evita atualizações repetidas
+        }
+    }
 }
